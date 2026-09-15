ab3361052a4e371755ffbf16ad8c10f7c36fda61
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-09-15 10:54:20 +0530

    fix: remove extra files

--- src/module/guest/dto/sign-in-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class SignInResponseDto {
-  @ApiProperty({example: 'We have sent you an OTP to verify account',})
-  message: string;
-
-  @ApiProperty({example: 'arjun@example.com',})
-  email: string;
-}
