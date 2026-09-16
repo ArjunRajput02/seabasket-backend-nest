ee433f32af5311ee8b1c7c3ed094d45c89d5100e
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-09-16 15:53:26 +0530

    fix: remove verified_at as it is not used

--- src/entities/user-verification.entity.ts
+++ src/entities/user-verification.entity.ts
@@ -29,10 +29,6 @@ export class UserVerificationEntity {
   @ApiProperty()
   expiresAt: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
-  @ApiProperty({ required: false, nullable: true })
-  verifiedAt: Date | null;
-
   @CreateDateColumn()
   @ApiProperty()
   createdAt: Date;
